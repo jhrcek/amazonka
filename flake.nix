ad01024f1201d97e9dc7198d7d50757b2656867f
Jan Hrček <honza.hrk@gmail.com>
2026-04-30 10:55:26 +0200

    Update flake lock, add ghc 9.12 dev shell

--- flake.nix
+++ flake.nix
@@ -46,6 +46,7 @@
         ghc94 = pkgs.haskell.packages."ghc94";
         ghc96 = pkgs.haskell.packages."ghc96";
         ghc98 = pkgs.haskell.packages."ghc98";
+        ghc912 = pkgs.haskell.packages."ghc912";
 
         # The default ghc to use when entering `nix develop`.
         ghcDefault = ghc94;
@@ -145,6 +146,7 @@
           ghc94 = mkDevShell ghc94;
           ghc96 = mkDevShell ghc96;
           ghc98 = mkDevShell ghc98;
+          ghc912 = mkDevShell ghc912;
           default = mkDevShell ghcDefault;
         };
       });
